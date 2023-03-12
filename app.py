0799dd699cc365f963567e938609f04bb641240b
Juniooor <juniormodern@hotmail.com>
2023-03-12 17:38:59 -0300

    fazendo scraping

--- app.py
+++ app.py
@@ -44,15 +44,15 @@ def new_product(sql, conexao, name, price, site, link_image, quote_date):
 # new_product(sql, conexao, 'Xbox Series X', 4000.59, 'https://www.xbox.com/pt-BR/consoles/xbox-series-x', datetime.now(), 'https://i.ibb.co/Lt6WnJ8/console-microsoft-xbox-series-x-1tb-preto-rrt-00006-1601067024-g.jpg' )
 conexao.commit()    
 
+
 def start_driver():
     chrome_options = Options()
-    
+     #--headless    
     arguments = ['--lang=en-US', '--window-size=1920,1080',
-                 '--incognito', ''] #--headless
-    
-    for argument in arguments:
-        chrome_options.add_argument(argument)
+                 '--incognito', ] 
         
+    for argument in arguments:
+        chrome_options.add_argument(argument)     
         
     chrome_options.add_experimental_option('prefs', {
         'download.prompt_for_download': False, 
@@ -74,15 +74,24 @@ def start_driver():
     )
     return driver, wait
 
-item = 'iphone 14 pro max'
-item = item.replace(' ', '-')
+
 
 def scan_site_1(item):
     driver, wait = start_driver()
-    driver.get('https://www.americanas.com.br/busca'+item)
+    driver.get('https://www.americanas.com.br/busca/'+item)
+    names = driver.find_elements(By.XPATH, "//div[@class='product-info__Container-sc-1or28up-0 cdKgxb']/h3")
+    price = driver.find_elements(By.XPATH, '//div[@class="price-info__Wrapper-sc-1xm1xzb-0 clqFWq inStockCard__PriceInfoUI-sc-1ngt5zo-3 ciWwot"]/span[@class="src__Text-sc-154pg0p-0 price__PromotionalPrice-sc-h6xgft-1 ctBJlj price-info__ListPriceWithMargin-sc-1xm1xzb-2 liXDNM"]' )
+    site =  driver.current_url
+    links_images = driver.find_elements(By.XPATH, '//div[@class="src__Wrapper-sc-xr9q25-1 ebFfaU"]/picture[@class="src__Picture-sc-xr9q25-2 ghIIuE"]/img')
     
 def scan_site_2():
     pass
 
 def scan_site_3():
     pass
+
+
+
+item = 'iphone 14 pro max'
+prod = item.replace(' ', '-')
+scan_site_1(prod)
